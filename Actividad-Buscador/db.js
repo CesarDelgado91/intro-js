05cd2951eb04516c7ce83486ebcf28645221644d
CesarDelgado <tommydr53@gmail.com>
2022-07-21 17:58:58 -0500

    buscador

---
+++ Actividad-Buscador/db.js
@@ -0,0 +1,57 @@
+const company = [
+{
+    nombre:"High-Back Bench",
+    modelo:"ikea",
+    precio:"9.90",
+    imagen:"https://www.hayesgardenworld.co.uk/sites/default/files/styles/product_large/public/images/products/main/309_0.jpg?itok=_eld2NJS",
+},
+{
+    nombre:"Albany Table",
+    modelo:"marcos",
+    precio:"79.99",
+    imagen:"https://secure.img1-fg.wfcdn.com/im/38830419/compr-r85/1854/185464012/rehoboth-bench.jpg",
+},
+{
+    nombre:"Accesnt Chair",
+    modelo:"caressa",
+    precio:"25.99",
+    imagen:"https://secure.img1-fg.wfcdn.com/im/38830419/compr-r85/1854/185464012/rehoboth-bench.jpg"
+},
+{
+    nombre:"Wooden Table",
+    modelo:"liddy",
+    precio:"45.99",
+    imagen:"https://secure.img1-fg.wfcdn.com/im/38830419/compr-r85/1854/185464012/rehoboth-bench.jpg"
+},
+{
+    nombre:"Dining Table",
+    modelo:"ikea",
+    precio:"6.99",
+    imagen:"https://secure.img1-fg.wfcdn.com/im/38830419/compr-r85/1854/185464012/rehoboth-bench.jpg"
+},
+{
+    nombre:"Sofa Set",
+    modelo:"marcus",
+    precio:"69.99",
+    imagen:"https://secure.img1-fg.wfcdn.com/im/38830419/compr-r85/1854/185464012/rehoboth-bench.jpg"
+},
+{
+    nombre:"Modern Bookshelf",
+    modelo:"caressa",
+    precio:"8.99",
+    imagen:"https://secure.img1-fg.wfcdn.com/im/38830419/compr-r85/1854/185464012/rehoboth-bench.jpg"
+},
+{
+    nombre:"Emperor Bed",
+    modelo:"liddy",
+    precio:"21.99",
+    imagen:"https://secure.img1-fg.wfcdn.com/im/38830419/compr-r85/1854/185464012/rehoboth-bench.jpg"
+},
+{
+    nombre:"Utopia Sofa",
+    modelo:"ikea",
+    precio:"39.95",
+    imagen:"https://secure.img1-fg.wfcdn.com/im/38830419/compr-r85/1854/185464012/rehoboth-bench.jpg"
+},
+]
+;
